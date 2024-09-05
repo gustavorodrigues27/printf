a1e1361ff78730a25f227f8ae38ba09eb10955d8
gustavorodrigues27 <goncalves.gustavo27@escola.pr.gov.br>
2024-09-05 10:49:27 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    String nome = "tekomonakama";
+    int dia = 8;
+    int mes = 7;
+    int ano = 2008;
+    double pi = 3.14159;
+
+    System.out.printf("o valor de pi é:  %.2f ", pi);
+      }
+
+}
